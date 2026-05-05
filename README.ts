3461185819b37b91339f776edf7353fdc595beb3
Alexander Zambrano <jesuscarrero182004@gmail.com>
2026-05-05 18:47:08 -0400

    nuevo commit

---
+++ README.ts
@@ -0,0 +1,28 @@
+   # MPV MENTE EN CALMA 
+
+
+ # Problema:
+Muchas veces como estudiante no te sientes cómodo hablando personalmente con tu psicólogo, te incomoda y terminas incomodado y con las mismas frustraciones y pensamientos que no le contaste a tu psicóloga. Por eso se creó la MPV “Mente en Calma”. 
+	
+ # Solución:
+La MPV “Mente en Calma”. trata de un chat anónimo donde el usuario puede tener una conversación con un profesional, (siendo los pasantes del área de psicología), que al mismo tiempo de que ayudan a los estudiantes también puedan realizar sus pasantías, esto será un benéfico doble, esta MPV tendrá un plus, teniendo también un chat extra donde la persona podrá hablar con otros usuarios, permitiendo relajarse formar amistades, distraerse y despejar la mente hablando con otros usuarios.
+	
+ # Características:
+1.	Un chat completamente anónimo donde pueden estar tranquilos.
+2.	Cuenta con profesionales que te van ayudar en todo momento. 
+3.	Un segundo chat donde puedes hablar con otros usuarios para formar amistades y relajarse.  
+
+ # Que no va hacer:
+Un quizz psicológico donde solo respondes formularios.
+
+ # Impacto Social:
+//Crea confianza a los usuarios que les incomoda ir al psicólogo, les permite abrirse, hablar y buscar ayuda sintiéndose más tranquilos y seguros, y, lo más importante con profesionales. Además, te ofrece un lugar donde podrás distraer tu mente creando nuevos vínculos, mediante un chat adicional donde puedes interactuar con los demás usuarios. 
+
+
+interface estudi {
+  name facultad: string
+
+
+
+
+}
